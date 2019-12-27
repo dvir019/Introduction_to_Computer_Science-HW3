b19480604931d5dc20ae084f8dce80b99f723781
Dvir <dvir019@gmail.com>
2019-12-27 11:38:10 +0200

    Add methods for the seed and the welcome message

--- hw3q1.py
+++ hw3q1.py
@@ -1,5 +1,17 @@
+import random
+
 def main():
-    pass
+    print_welcome_message()
+    get_and_set_seed()
+
+def print_welcome_message():
+    print('Welcome to Battleship!')
+
+def get_and_set_seed():
+    print('Please enter seed:')
+    seed = int(input())
+    random.seed(seed)
+
 
 if __name__ == '__main__':
     main()
